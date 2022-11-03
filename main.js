9fd98b67f8bbc1dc242ae713be07d49ec443ed10
camillabrizzi <camillabrizzi@icloud.com>
2022-11-03 15:23:32 +0000

    kata ready to go!

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 /* 
-👉 Write your kata here!
+
+MonKee is hungry and looking to jump up some trees to fetch bananas for lunch. It can only jump one or three trees up at a time. Work out the minimum number of jumps required for the monkey to get from start to finish and return an array containing as many bananas as the number of jumps: e.g. 4 jumps -> ["banana", "banana", "banana", "banana"]
+                 
+                       🌳 6
+                 
+     
+🌳 5       
+           ┌─────► OK! 🌳 4
+           │    
+           │      
+           │   
+           |
+🌳 3       |
+NO!        ├─────► OK! 🌳 2
+  ▲        │   
+  │        │     
+  │        │  
+🌳 1  🐒
+
 */
 
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+
+
+export default function bananas(start, finish) {
+    //write your code here!
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,44 @@
 //👉 Write your tests below here:
+
+//return correct number of jumps
+//return array containing correct number of bananas as separate strings
+
+import bananas from "./main"
+
+test("Number of minimum jumps matches number of 'banana' strings in array returned", () => {
+    const actual = bananas(2,7);
+    const expected = ["banana", "banana", "banana"];
+    expect(actual).toStrictEqual(expected);
+});
+
+test("Number of minimum jumps matches number of 'banana' strings in array returned", () => {
+    const actual = bananas(3,11);
+    const expected = ['banana', 'banana', 'banana', 'banana'];
+    expect(actual).toStrictEqual(expected);
+});
+
+test("Number of minimum jumps matches number of 'banana' strings in array returned", () => {
+    const actual = bananas(7,15);
+    const expected = ['banana', 'banana', 'banana', 'banana'];
+    expect(actual).toStrictEqual(expected);
+});
+
+test("Number of minimum jumps matches number of 'banana' strings in array returned", () => {
+    const actual = bananas(1,32);
+    const expected = ['banana', 'banana', 'banana', 'banana', 'banana', 'banana', 'banana', 'banana', 'banana', 'banana', 'banana'];
+    expect(actual).toStrictEqual(expected);
+});
+
+test("Number of minimum jumps matches number of 'banana' strings in array returned", () => {
+    const actual = bananas(0,0);
+    const expected = [];
+    expect(actual).toStrictEqual(expected);
+});
+
+test("Number of minimum jumps matches number of 'banana' strings in array returned", () => {
+    const actual = bananas(0,1);
+    const expected = ["banana"];
+    expect(actual).toStrictEqual(expected);
+});
+
+
