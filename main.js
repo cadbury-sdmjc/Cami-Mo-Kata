9c985f6ed29984dde34796b47d66e91eb9427ff3
camillabrizzi <camillabrizzi@icloud.com>
2022-11-03 15:34:24 +0000

    updated description

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* 
 
-MonKee is hungry and looking to jump up some trees to fetch bananas for lunch. It can only jump one or three trees up at a time. Work out the minimum number of jumps required for the monkey to get from start to finish and return an array containing as many bananas as the number of jumps: e.g. 4 jumps -> ["banana", "banana", "banana", "banana"]
+MonKee is hungry and looking to jump up some trees to fetch bananas for lunch. It can only jump one or three trees up at a time. Work out the minimum number of jumps required for the monkey to get from start to finish and return an array containing as many bananas as the number of jumps: e.g. 4 jumps -> ["banana", "banana", "banana", "banana"]. The start and end numbers will always be integers (incl 0)
                  
                        🌳 6
                  
